56767864bb60e9132dc037aeb771b7de5fb55e8f
nek00oo <yakovenkovalera2017@yandex.ru>
2024-10-02 11:00:19 +0300

    added a declaration of CRUD methods

--- UserService/Services/UserApiService.cs
+++ UserService/Services/UserApiService.cs
@@ -1,11 +1,31 @@
 using Grpc.Core;
 
 namespace UserService.Services;
-
 public class UserApiService : UserService.UserServiceBase
 {
-    //Должно быть типо так
-    /*public override Task<UserReply> GetUserById(GetUserByIdRequest request, ServerCallContext context)
+    
+    public override Task<UserReply> GetUserById(GetUserByIdRequest request, ServerCallContext context)
     {
-    }*/
+        return base.GetUserById(request, context);
+    }
+
+    public override Task<UserReply> GetUserByName(GetUserByNameRequest request, ServerCallContext context)
+    {
+        return base.GetUserByName(request, context);
+    }
+
+    public override Task<UserReply> CreateUser(CreateUserRequest request, ServerCallContext context)
+    {
+        return base.CreateUser(request, context);
+    }
+
+    public override Task<UserReply> UpdateUser(UpdateUserRequest request, ServerCallContext context)
+    {
+        return base.UpdateUser(request, context);
+    }
+
+    public override Task<UserReply> DeleteUser(DeleteUserRequest request, ServerCallContext context)
+    {
+        return base.DeleteUser(request, context);
+    }
 }
